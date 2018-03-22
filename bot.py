1777dc492aa82e0e01e970e10b9e68cc01d0cbc8
Jane Jeon <JaneJeon9719@gmail.com>
2018-03-22 11:54:13 -0700

    Ordered logs

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 from discord import Client
 
+# replace with channels you want to scrape
+CHANNELS_TO_SCRAPE = ['treehouse', 'another-place-to-talk', 'secret-channel']
+
 client = Client()
 
 @client.event
@@ -8,24 +11,40 @@ async def on_ready():
 
 @client.event
 async def on_message(msg):
-	log_message(msg)
+	log_message(msg, True)
 
-def log_message(msg):
+def log_message(msg, stdout=False):
+	line = log_format(msg)
+	
 	with open('log.txt', 'a') as log:
-		log.write(f'[{msg.timestamp}][{msg.channel}] @{msg.author}: {msg.clean_content}\n')
+		log.write(line)
+	if stdout:
+		print(line)
 
-async def scrape_messages():
+def log_format(msg):
+	# discord API only allows one embed per comment
+	embed = f'[{msg.embeds[0].url}]' if msg.embeds else ''
+	attachment = f'[{msg.attachments[0]["url"]}]' if msg.attachments else ''
+	return f'[{msg.timestamp}][{msg.channel}]{attachment}{embed} @{msg.author}: {msg.clean_content}\n'
+
+# run once on bot startup
+async def scrape_messages(chronological=True):
 	await client.wait_until_ready()
+	messages = []
 	
 	for channel in client.get_all_channels():
-		# replace with channels you want to scrape ▼
-		if str(channel) in ['treehouse', 'another-place-to-talk', 'secret-channel']:
+		if str(channel) in CHANNELS_TO_SCRAPE:
 			try:
 				async for message in client.logs_from(channel, limit=1e20, reverse=True):
-					log_message(message)
+					messages.append(message) if chronological else log_message(message)
 				print(f'Scraped {channel}')
 			except Exception as e:
 				print(e)
+	
+	if chronological:
+		messages.sort(key=lambda x: x.timestamp)
+		for message in messages:
+			log_message(message)
 
 client.change_presence(status='invisible')
 client.loop.create_task(scrape_messages())
